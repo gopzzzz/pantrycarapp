dbf1a00fd23d39919e8c7afc6b8a5ed4173cceb7
gopikaroadmate <gopika@roadmate.in>
2024-07-29 23:51:09 -0700

    first initial push

---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,30 @@
+<!-- Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2023 <a href="#">Roadmate</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.0.0
+    </div>
+  </footer>
+<!-- Footer -->
+<!-- Due Payment Modal -->
+<div class="modal fade" id="duepayModal" tabindex="-1" role="dialog" aria-labelledby="duepayModal" aria-hidden="true">
+  <div class="modal-dialog modal-md" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="duepayModal"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="alert alert-danger" role="alert" id="alert">
+            
+        </div>
+        
+      </div>
+     
+    </div>
+  </div>
+</div>
+<!-- Due Payment Modal -->
